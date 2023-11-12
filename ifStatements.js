ae7b368f306e1c9550ddcfeff04c4d38b8e0da5e
Megan Crotteau <megan.crotteau@gmail.com>
2023-11-12 12:52:05 -0700

    Add conditions for gumballs and pizza

--- ifStatements.js
+++ ifStatements.js
@@ -8,10 +8,17 @@
 // Write a conditional statement that prints only one or the other.
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
-var numQuarters = 0;
+var numQuarters = 3;
 
+console.log("Can I buy a gumball??", numQuarters >= 2);
+
+if (numQuarters >= 2) {
 console.log("I have enough money for a gumball");
+} else {
 console.log("I don't have enough money for a gumball");
+}
+
+
 
 
 // CHALLENGE 2
@@ -26,6 +33,19 @@ console.log("I don't have enough money for a gumball");
 
 // Experiment with manipulating the value held within both variables to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 2;
 var hasSauce = true;
 
+if ((cupsOfFlour === 1) && (hasSauce === true)) {
+ console.log("I cannot make pizza");
+} else if ((cupsOfFlour === 5) && (hasSauce === false)) {
+    console.log("I cannot make pizza"); 
+} else if ((cupsOfFlour === 2) && (hasSauce === true)) {
+    console.log("I can make pizza!!");
+} else if ((cupsOfFlour === 3) && (hasSauce === true)) {
+    console.log("I can make pizza!");
+} else if ((cupsOfFlour >= 2) && (hasSauce === true)) {
+   console.log("I can definitely make pizza!!");
+} else if ((cupsOfFlour < 2) || (hasSauce === false)) {
+   console.log("No pizza for us today :(");
+}
