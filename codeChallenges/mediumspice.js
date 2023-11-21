12bf995df439a4cab9894cc1a441a424ec8ebabd
Megan Crotteau <megan.crotteau@gmail.com>
2023-11-20 22:40:49 -0700

    code that works

--- codeChallenges/mediumspice.js
+++ codeChallenges/mediumspice.js
@@ -8,9 +8,9 @@ var goodDrivingRecord = true;
 var age = 24;
 
 if ((goodDrivingRecord === true) && (age > 25)) {
-    console.log("You should get a discount on the car rental")
-} else if ((goodDrivingRecord ==== true) || (age >  25)) {
-    console.log("They should pay full price")
-} else ((goodDrivingRecord === false) && (age <= 25)) {
-    console.log("They need to have someone else sign for the rental")
+    console.log("You should get a discount on the car rental");
+} else if ((goodDrivingRecord === true) || (age >  25)) {
+    console.log("They should pay full price");
+} else if ((goodDrivingRecord === false) && (age <= 25)) {
+    console.log("They need to have someone else sign for the rental");
 }
